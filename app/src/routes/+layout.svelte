413b7d5417753871f0e943e871c6102af6d551d9
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-04-05 23:51:38 +0200

    layout

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<svelte:head>
+
+</svelte:head>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<p>Welcome to SvelteKit!</p>
+
+<style>
+    @import "tailwindcss";
+
+    body{
+        background-image: url('');
+    }
+</style>
