d67fa60a6f26c7b4356f2ece9fd41b1a31bcea32
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-04-06 23:11:51 +0200

    navbar & bg

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,7 +1,34 @@
 <script>
-
+import '../app.css';
+import { Bell, UserRound, ChevronDown, Search } from '@lucide/svelte';
 </script>
 
-<svelte:head>
+<nav class="flex justify-evenly ">
+    <div class="bg-transparent flex items-center">
+        <Search/>
+        <input type="text" placeholder="Search movies " class="ml-20" />
+    </div>
+    <ul class="">
+        <li>Movie</li>
+        <li>TV Series</li>
+        <li>Animations</li>
+        <li>Mistery</li>
+    </ul>
+    <div>
+        <Bell size="24" color="white" />
+        <div>
+            <div>
+                <UserRound size="24" color="white" />
+                <span>Pseudo</span><br>
+                <span>pseudo@gmail.com</span>
+            </div>
+            <ChevronDown size="24" color="white" />
+        </div>
+    </div>
+</nav>
 
-</svelte:head>
+<slot />
+
+<style>
+
+</style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,12 +2,29 @@
 
 </script>
 
-<p>Welcome to SvelteKit!</p>
+<body>
+<main>
+    <section>
+        <article>
+            <div>
+
+            </div>
+        </article>
+    </section>
+    <section>
+
+    </section>
+</main>
+</body>
 
 <style>
-    @import "tailwindcss";
-
-    body{
-        background-image: url('');
+    body {
+        height: 100vh;
+        width: 100%;
+        z-index: -1;
+        background-image: url('../assets/images/Apple-visionOS-2-hand-gestures.jpg.large_2x.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
     }
 </style>
