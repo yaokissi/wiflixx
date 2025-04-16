709795df28a94797193179f4c1097592c90d830e
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-04-16 23:36:21 +0200

    style: new trailer bloc finish

--- app/src/lib/components/NewTrailerSection.svelte
+++ app/src/lib/components/NewTrailerSection.svelte
@@ -11,42 +11,111 @@
                 backdrop-blur-2xl
                 rounded-xl
                 border-white-500
-                h-[30vh]
-
-
+                h-[50vh]
+                overflow-auto
         ">
     <div class="flex
                 justify-between items-center w-[90%]
-                mx-auto">
-        <h2 class="text-sm">New Trailer</h2>
-        <p class="text-sm"><span class="text-gray-700 text-xs">Sort By :</span> Today </p>
-        <ArrowDownWideNarrow
-                color="white"
-                class="
-            text-white
-            inline-flex
-            items-center
-            justify-center"
-        >
-        </ArrowDownWideNarrow>
+                mx-auto
+                pt-5
+">
+        <h2 class="text-sm text-white">🔥 New Trailer</h2>
+        <p class="text-sm text-white"><span class="text-gray-700 text-xs">Sort By :</span> Today ⏚
+
+        </p>
+
     </div>
 
     <div class="
                 flex
-                justify-between items-center w-[90%]
+                justify-between items-center
+                w-[90%]
+                rounded-xl
                 mx-auto
                 mt-5
                 h-[20vh]
+                relative
                 bg-cover
                 bg-no-repeat
                 bg-center
+
 
 "
             style="
             background-image: url({imageUrl})
 
             ;">
-        <div class="absolute bottom-0 p-2 text-sm z-10  backdrop-blur-md rounded-b-xl  ">
+        <div class="absolute bottom-0 text-sm text-white backdrop-blur-2xl mx-auto p-2 rounded-b-xl  ">
+            BMF: Black Mafia Family, Mexico Edition
+        </div>
+    </div>
+
+    <div class="
+                flex
+                justify-between items-center
+                w-[90%]
+                rounded-xl
+                mx-auto
+                mt-5
+                h-[20vh]
+                relative
+                bg-cover
+                bg-no-repeat
+                bg-center
+
+
+"
+         style="
+            background-image: url({imageUrl})
+
+            ;">
+        <div class="absolute bottom-0 text-sm text-white backdrop-blur-2xl mx-auto p-2 rounded-b-xl  ">
+            BMF: Black Mafia Family, Mexico Edition
+        </div>
+    </div>
+    <div class="
+                flex
+                justify-between items-center
+                w-[90%]
+                rounded-xl
+                mx-auto
+                mt-5
+                h-[20vh]
+                relative
+                bg-cover
+                bg-no-repeat
+                bg-center
+
+
+"
+         style="
+            background-image: url({imageUrl})
+
+            ;">
+        <div class="absolute bottom-0 text-sm text-white backdrop-blur-2xl mx-auto p-2 rounded-b-xl  ">
+            BMF: Black Mafia Family, Mexico Edition
+        </div>
+    </div>
+    <div class="
+                flex
+                justify-between items-center
+                w-[90%]
+                rounded-xl
+                mx-auto
+                mt-5
+                h-[20vh]
+                relative
+                bg-cover
+                bg-no-repeat
+                bg-center
+
+
+"
+         style="
+            background-image: url({imageUrl})
+
+            ;">
+        <div class="absolute bottom-0 text-sm text-white backdrop-blur-2xl mx-auto p-2 rounded-b-xl  ">
             BMF: Black Mafia Family, Mexico Edition
         </div>
     </div>
