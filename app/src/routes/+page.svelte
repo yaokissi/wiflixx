b46c3d42cd9ce5fc16b9ead940785cfbc262f4eb
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-04-22 04:29:02 +0200

    final mvp wiflix app #2

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -14,7 +14,7 @@ import Popup from '$lib/components/ui/Popup.svelte';
         height: 100vh;
         width: 100%;
         backdrop-filter: blur(5px);
-        background-image: url("../assets/images/mindfulness_startframe__c070kjbtkqky_large.jpg");
+        background-image: url("../assets/images/Apple-visionOS-2-hand-gestures.jpg.large_2x.jpg");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
